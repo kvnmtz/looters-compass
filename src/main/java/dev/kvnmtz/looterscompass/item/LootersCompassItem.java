77b589e1910a919a9901843d9a738e9568671af9
Kevin <kevinmatz.km@gmail.com>
2025-10-19 00:35:29 +0200

    perf: improve container search performance

--- src/main/java/dev/kvnmtz/looterscompass/item/LootersCompassItem.java
+++ src/main/java/dev/kvnmtz/looterscompass/item/LootersCompassItem.java
@@ -22,6 +22,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.DistExecutor;
@@ -31,15 +32,18 @@ import noobanidus.mods.lootr.entity.LootrChestMinecartEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Objects;
 import java.util.UUID;
 
 import java.util.List;
 
+@ParametersAreNonnullByDefault
 public class LootersCompassItem extends Item {
+
     private static final String FOUND_POS_TAG = "FoundPos";
-    private static final String FOUND_ENTITY_UUID_TAG = "FoundEntityUUID";
-    private static final String IS_TRACKING_ENTITY_TAG = "IsTrackingEntity";
+
+    private static final int SEARCH_CACHE_DURATION_TICKS = 100;
 
     public LootersCompassItem() {
         super(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.UNCOMMON));
@@ -49,6 +53,11 @@ public class LootersCompassItem extends Item {
 
     @OnlyIn(Dist.CLIENT)
     private static class LootersCompassItemClient {
+        private static final ClientCompassData CACHE = new ClientCompassData();
+        
+        public static ClientCompassData getCache() {
+            return CACHE;
+        }
 
         public static void registerItemProperties(LootersCompassItem item) {
             ItemProperties.register(item, ResourceLocation.parse("angle"),
@@ -68,10 +77,20 @@ public class LootersCompassItem extends Item {
                         return foundPos == null ? 1.0f : 0.0f;
                     });
         }
+        
+        public static class ClientCompassData {
+            public UUID trackedEntityUUID;
+            public boolean isTrackingEntity;
+            public BlockPos lastSearchPos;
+            public BlockPos lastSearchResult;
+            public int lastSearchTime;
+            
+            public ClientCompassData() {}
+        }
     }
 
     @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand usedHand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
         var stack = player.getItemInHand(usedHand);
 
         if (!ServerConfig.ENABLE_LOOT_CONTAINER_GLOW.get()) {
@@ -79,13 +98,11 @@ public class LootersCompassItem extends Item {
         }
 
         if (level.isClientSide) {
-            if (isTrackingEntity(stack)) {
-                var entityUUID = getFoundEntityUUID(stack);
-                if (entityUUID != null) {
-                    var entity = findEntityByUUID((ClientLevel) level, entityUUID);
-                    if (entity instanceof LootrChestMinecartEntity minecart) {
-                        CompassGlowManager.activateGlowForEntity(minecart);
-                    }
+            var cache = LootersCompassItemClient.getCache();
+            if (cache.isTrackingEntity && cache.trackedEntityUUID != null) {
+                var entity = findEntityByUUID((ClientLevel) level, cache.trackedEntityUUID);
+                if (entity instanceof LootrChestMinecartEntity minecart) {
+                    CompassGlowManager.activateGlowForEntity(minecart);
                 }
             } else {
                 var targetPos = getFoundPosition(stack);
@@ -106,25 +123,25 @@ public class LootersCompassItem extends Item {
     }
 
     @Override
-    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag isAdvanced) {
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
         tooltipComponents.add(Component.translatable("item.looters_compass.looters_compass.desc").withStyle(ChatFormatting.GRAY));
         super.appendHoverText(stack, level, tooltipComponents, isAdvanced);
     }
 
     @Override
-    public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int slotId, boolean isSelected) {
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
         if (!level.isClientSide) return;
         if (!(entity instanceof Player player)) return;
 
-        if (isTrackingEntity(stack)) {
-            var entityUUID = getFoundEntityUUID(stack);
-            if (entityUUID != null) {
-                var trackedEntity = findEntityByUUID((ClientLevel) level, entityUUID);
-                if (trackedEntity != null) {
-                    setFoundPosition(stack, trackedEntity.blockPosition());
-                } else {
-                    clearFoundPosition(stack);
-                }
+        var cache = LootersCompassItemClient.getCache();
+        if (cache.isTrackingEntity && cache.trackedEntityUUID != null) {
+            var trackedEntity = findEntityByUUID((ClientLevel) level, cache.trackedEntityUUID);
+            if (trackedEntity != null) {
+                setFoundPosition(stack, trackedEntity.blockPosition());
+            } else {
+                clearFoundPosition(stack);
+                cache.isTrackingEntity = false;
+                cache.trackedEntityUUID = null;
             }
         }
 
@@ -132,10 +149,26 @@ public class LootersCompassItem extends Item {
         searchForLootContainers(stack, level, player);
     }
 
-    private void searchForLootContainers(@NotNull ItemStack stack, @NotNull Level level, @NotNull Player player) {
+    @OnlyIn(Dist.CLIENT)
+    private void searchForLootContainers(ItemStack stack, Level level, Player player) {
         var playerPos = player.blockPosition();
-        var nearestDistance = Double.MAX_VALUE;
+        var cache = LootersCompassItemClient.getCache();
 
+        var playerDidntMoveMuch = cache.lastSearchPos != null && playerPos.distSqr(cache.lastSearchPos) <= 9;
+        var isCacheApplicable = cache.lastSearchTime > 0 && player.tickCount - cache.lastSearchTime <= SEARCH_CACHE_DURATION_TICKS && playerDidntMoveMuch;
+
+        if (isCacheApplicable) {
+            if (cache.lastSearchResult != null) {
+                cache.isTrackingEntity = false;
+                cache.trackedEntityUUID = null;
+                setFoundPosition(stack, cache.lastSearchResult);
+            } else {
+                clearFoundPosition(stack);
+            }
+            return;
+        }
+
+        var nearestDistance = Double.MAX_VALUE;
         var maxRadiusXZ = ServerConfig.SEARCH_RADIUS_XZ.get();
         var maxRadiusY = ServerConfig.SEARCH_RADIUS_Y.get();
 
@@ -149,50 +182,71 @@ public class LootersCompassItem extends Item {
             var minecartDistance = playerPos.distSqr(nearestMinecart.blockPosition());
             if (minecartDistance < nearestDistance) {
                 setFoundEntity(stack, nearestMinecart);
+                cacheSearchResult(nearestMinecart.blockPosition(), playerPos, player.tickCount);
                 return;
             }
         }
 
         if (nearestContainer != null) {
-            var tag = stack.getOrCreateTag();
-            tag.remove(FOUND_ENTITY_UUID_TAG);
-            tag.remove(IS_TRACKING_ENTITY_TAG);
+            cache.isTrackingEntity = false;
+            cache.trackedEntityUUID = null;
             setFoundPosition(stack, nearestContainer);
+            cacheSearchResult(nearestContainer, playerPos, player.tickCount);
         } else {
             clearFoundPosition(stack);
+            cacheSearchResult(null, playerPos, player.tickCount);
         }
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Nullable
-    private BlockPos searchLootrContainers(@NotNull Level level, @NotNull Player player, @NotNull BlockPos playerPos, int maxRadiusXZ, int maxRadiusY) {
+    private BlockPos searchLootrContainers(Level level, Player player, BlockPos playerPos, int maxRadiusXZ, int maxRadiusY) {
         BlockPos nearestContainer = null;
-        double nearestDistanceSq = Double.MAX_VALUE;
+        var nearestDistanceSq = Double.MAX_VALUE;
 
-        for (var radius = 1; radius <= maxRadiusXZ; radius++) {
-            for (var x = -radius; x <= radius; x++) {
-                for (var z = -radius; z <= radius; z++) {
-                    var isPositionAtEdgeOfRadius = Math.abs(x) == radius || Math.abs(z) == radius;
-                    if (!isPositionAtEdgeOfRadius) continue;
+        // convert blocks to chunks (add one to be safe)
+        var chunkRadius = (maxRadiusXZ >> 4) + 1;
+        var playerChunkX = playerPos.getX() >> 4;
+        var playerChunkZ = playerPos.getZ() >> 4;
 
-                    for (var y = -maxRadiusY; y <= maxRadiusY; y++) {
-                        var checkPos = playerPos.offset(x, y, z);
-                        if (isUnopenedLootrContainer(level, checkPos, player)) {
-                            var distanceSq = playerPos.distSqr(checkPos);
-                            if (distanceSq < nearestDistanceSq) {
-                                nearestDistanceSq = distanceSq;
-                                nearestContainer = checkPos;
-                            }
-                        }
+        for (var chunkX = playerChunkX - chunkRadius; chunkX <= playerChunkX + chunkRadius; chunkX++) {
+            for (var chunkZ = playerChunkZ - chunkRadius; chunkZ <= playerChunkZ + chunkRadius; chunkZ++) {
+                var isChunkLoaded = level.isLoaded(new BlockPos(chunkX << 4, playerPos.getY(), chunkZ << 4));
+                if (!isChunkLoaded) continue;
+                
+                var chunk = level.getChunk(chunkX, chunkZ);
+
+                var blockEntities = chunk.getBlockEntities();
+                for (var entry : blockEntities.entrySet()) {
+                    var pos = entry.getKey();
+                    var blockEntity = entry.getValue();
+
+                    if (!isUnopenedLootrContainer(blockEntity, player)) continue;
+
+                    var dx = pos.getX() - playerPos.getX();
+                    var dy = pos.getY() - playerPos.getY();
+                    var dz = pos.getZ() - playerPos.getZ();
+
+                    var isWithinSearchRadius = Math.abs(dx) <= maxRadiusXZ && Math.abs(dz) <= maxRadiusXZ && Math.abs(dy) <= maxRadiusY;
+                    if (!isWithinSearchRadius) {
+                        continue;
+                    }
+
+                    var distanceSq = dx * dx + dy * dy + dz * dz;
+                    if (distanceSq < nearestDistanceSq) {
+                        nearestDistanceSq = distanceSq;
+                        nearestContainer = pos;
                     }
                 }
             }
         }
-
+        
         return nearestContainer;
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Nullable
-    private LootrChestMinecartEntity searchLootrMinecarts(@NotNull Level level, @NotNull BlockPos playerPos, int maxRadiusXZ, int maxRadiusY) {
+    private LootrChestMinecartEntity searchLootrMinecarts(Level level, BlockPos playerPos, int maxRadiusXZ, int maxRadiusY) {
         var searchBox = new AABB(
                 playerPos.getX() - maxRadiusXZ, playerPos.getY() - maxRadiusY, playerPos.getZ() - maxRadiusXZ,
                 playerPos.getX() + maxRadiusXZ, playerPos.getY() + maxRadiusY, playerPos.getZ() + maxRadiusXZ
@@ -217,17 +271,18 @@ public class LootersCompassItem extends Item {
         return nearestMinecart;
     }
 
-    private boolean isUnopenedLootrContainer(@NotNull Level level, @NotNull BlockPos pos, @NotNull Player player) {
-        var blockEntity = level.getBlockEntity(pos);
-        if (blockEntity == null) return false;
-
-        if (!(blockEntity instanceof ILootBlockEntity lootBlockEntity)) return false;
+    @OnlyIn(Dist.CLIENT)
+    private boolean isUnopenedLootrContainer(BlockEntity blockEntity, Player player) {
+        if (!(blockEntity instanceof ILootBlockEntity lootBlockEntity)) {
+            return false;
+        }
 
         return !lootBlockEntity.getOpeners().contains(player.getUUID());
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Nullable
-    private BlockPos getFoundPosition(@NotNull ItemStack stack) {
+    private BlockPos getFoundPosition(ItemStack stack) {
         var tag = stack.getTag();
         if (tag == null || !tag.contains(FOUND_POS_TAG)) return null;
 
@@ -239,7 +294,8 @@ public class LootersCompassItem extends Item {
         );
     }
 
-    private void setFoundPosition(@NotNull ItemStack stack, @NotNull BlockPos pos) {
+    @OnlyIn(Dist.CLIENT)
+    private void setFoundPosition(ItemStack stack, BlockPos pos) {
         var hasPositionChanged = getFoundPosition(stack) == null || Objects.requireNonNull(getFoundPosition(stack)).asLong() != pos.asLong();
         if (!hasPositionChanged)
             return;
@@ -256,43 +312,35 @@ public class LootersCompassItem extends Item {
         CompassGlowManager.clearGlow();
     }
 
-    private void clearFoundPosition(@NotNull ItemStack stack) {
+    @OnlyIn(Dist.CLIENT)
+    private void clearFoundPosition(ItemStack stack) {
         var tag = stack.getTag();
         if (tag != null) {
             tag.remove(FOUND_POS_TAG);
-            tag.remove(FOUND_ENTITY_UUID_TAG);
-            tag.remove(IS_TRACKING_ENTITY_TAG);
         }
 
         CompassGlowManager.clearGlow();
     }
 
-    private void setFoundEntity(@NotNull ItemStack stack, @NotNull Entity entity) {
-        var tag = stack.getOrCreateTag();
-        tag.putString(FOUND_ENTITY_UUID_TAG, entity.getUUID().toString());
-        tag.putBoolean(IS_TRACKING_ENTITY_TAG, true);
+    @OnlyIn(Dist.CLIENT)
+    private void setFoundEntity(ItemStack stack, Entity entity) {
+        var cache = LootersCompassItemClient.getCache();
+        cache.trackedEntityUUID = entity.getUUID();
+        cache.isTrackingEntity = true;
 
         setFoundPosition(stack, entity.blockPosition());
     }
-    
-    @Nullable
-    private UUID getFoundEntityUUID(@NotNull ItemStack stack) {
-        var tag = stack.getTag();
-        if (tag == null || !tag.contains(FOUND_ENTITY_UUID_TAG)) return null;
-        
-        try {
-            return UUID.fromString(tag.getString(FOUND_ENTITY_UUID_TAG));
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
-    
-    private boolean isTrackingEntity(@NotNull ItemStack stack) {
-        var tag = stack.getTag();
-        return tag != null && tag.getBoolean(IS_TRACKING_ENTITY_TAG);
-    }
-    
-    private @Nullable Entity findEntityByUUID(@NotNull ClientLevel level, @NotNull UUID uuid) {
+
+    @OnlyIn(Dist.CLIENT)
+    private @Nullable Entity findEntityByUUID(ClientLevel level, UUID uuid) {
         return ((ClientLevelAccessor) level).callGetEntities().get(uuid);
+    }
+    
+    @OnlyIn(Dist.CLIENT)
+    private void cacheSearchResult(@Nullable BlockPos result, BlockPos playerPos, int currentTick) {
+        var cache = LootersCompassItemClient.getCache();
+        cache.lastSearchPos = playerPos;
+        cache.lastSearchResult = result;
+        cache.lastSearchTime = currentTick;
     }
 }
